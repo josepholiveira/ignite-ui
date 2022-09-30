4a05d36aa0aee9d8e49653dfe3269f4887ff723d
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2022-09-30 15:22:35 -0300

    chore(docs): configure dark mode for storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,7 +1,30 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
 export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$ignite500',
-  borderRadius: '$md',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
+
+export type ButtonProps = ComponentProps<typeof Button>
